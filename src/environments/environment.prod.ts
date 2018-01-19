92d86e2a83214d9f1ceb31b01cd9e8d76fd027c0
Max Koning <m.koning4@student.avans.nl>
2018-01-19 03:28:27 +0100

    url goed gezet

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  nodeServerUrl: 'https://nodejs-groep-sportunite.herokuapp.com/api/v1/',
+  nodeServerUrl: 'https://nodejs-groep-sportunite.herokuapp.com/api/v1',
   nodeServerUrlChar: 'https://nodejs-groep-sportunite.herokuapp.com/api/v1/users/',
   webApiServerUrlRel: 'http://sportuniteapiavans.azurewebsites.net/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: true,
-  nodeServerUrl: 'https://nodejs-groep-sportunite.herokuapp.com/api/v1/',
+  nodeServerUrl: 'https://nodejs-groep-sportunite.herokuapp.com/api/v1',
   nodeServerUrlChar: 'https://nodejs-groep-sportunite.herokuapp.com/api/v1/users/',
   webApiServerUrlRel: 'http://sportuniteapiavans.azurewebsites.net/api'
 };
